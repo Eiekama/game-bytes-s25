0d5c4b6e762df7efec4278904a841eba72bb5480
Zachary Heintzleman <zheintzleman@gmail.com>
2025-02-16 16:00:16 -0500

    Powerup Things

--- Crucible/Assets/Minigames/FlyingHazard/Scripts/Powerup.cs
+++ Crucible/Assets/Minigames/FlyingHazard/Scripts/Powerup.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 namespace Minigames.FlyingHazard.Scripts
 {
+    public enum PowerupType
+    {
+        EnergyShield,
+        RiceMagnet,
+        LimeBoost,
+        Stopwatch,
+        MushroomFlip,
+        SwapWarp,
+        OneUp,
+        None
+    }
     public class Powerup : MonoBehaviour
     {
+        [SerializeField] public PowerupType type;
         
         private void OnTriggerEnter2D(Collider2D collider)
         {
             if (!collider.CompareTag("Player")) return;
-            
-            // TODO: do stuff
-            Debug.Log("Powerup triggered");
-            Destroy(gameObject);
+            GameObject player = collider.gameObject;
+            Debug.Log(type + " triggered by " + player.name);
         }
     }
 }
