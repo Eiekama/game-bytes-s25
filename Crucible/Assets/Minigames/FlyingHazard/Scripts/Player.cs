460ee3feb94b54e6ae2fe0aa39ee417f1f31f04b
justsomeboy26 <zhangjimmy31@gmail.com>
2025-04-24 22:57:08 -0400

    color

--- Crucible/Assets/Minigames/FlyingHazard/Scripts/Player.cs
+++ Crucible/Assets/Minigames/FlyingHazard/Scripts/Player.cs
@@ -392,7 +392,7 @@ namespace Minigames.FlyingHazard.Scripts
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForSeconds(0.2f);
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, .6f, 0f, 0f);
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, .6f, 0f, 1f);
                 yield return new WaitForSeconds(0.2f);
                 gameObject.GetComponent<SpriteRenderer>().color = Color.white;
             }
@@ -421,7 +421,7 @@ namespace Minigames.FlyingHazard.Scripts
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForSeconds(0.2f);
-                gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, .6f, 0f, 0f);
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, .6f, 0f, 1f);
                 yield return new WaitForSeconds(0.2f);
                 gameObject.GetComponent<SpriteRenderer>().color = Color.white;
             }
